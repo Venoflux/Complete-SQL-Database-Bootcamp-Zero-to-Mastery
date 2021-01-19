7f1211b433884f6f6679ca74b0abf3ba23404ecc
Venoflux <32600575+Venoflux@users.noreply.github.com>
2021-01-19 17:21:29 +0100

    Update questions.sql

--- SQL Deep Dive/Null Values/questions.sql
+++ SQL Deep Dive/Null Values/questions.sql
@@ -2,11 +2,13 @@
 /*
 * DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
 * Question: 
-* Assuming a students minimum age for the class is 15, what is the average age of a student?
+* Assuming a students minimum age for the class is 15, what is the average age of a student? A: 30
 */
-
+SELECT avg(coalesce(age, 15)) from "Student";
+                    
 /*
 * DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
 * Question: 
 * Replace all empty first or last names with a default?
 */
+SELECT id, coalesce(name, 'fallback'), coalesce(lastName, 'lastName'), age FROM "Student";
